e692ff6653880e5314bcba9af6c260278e38f093
sjv01180 <SamuelVelasquez@protonmail.com>
2019-04-17 06:50:40 -0400

    three button main menu skeleton

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/zxing/client/android/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/zxing/client/android/R.java
@@ -40,21 +40,21 @@ public final class R {
         private id() {}
 
         public static final int centerCrop = 0x7f070024;
-        public static final int fitCenter = 0x7f07003d;
-        public static final int fitXY = 0x7f07003e;
-        public static final int zxing_back_button = 0x7f070098;
-        public static final int zxing_barcode_scanner = 0x7f070099;
-        public static final int zxing_barcode_surface = 0x7f07009a;
-        public static final int zxing_camera_closed = 0x7f07009b;
-        public static final int zxing_camera_error = 0x7f07009c;
-        public static final int zxing_decode = 0x7f07009d;
-        public static final int zxing_decode_failed = 0x7f07009e;
-        public static final int zxing_decode_succeeded = 0x7f07009f;
-        public static final int zxing_possible_result_points = 0x7f0700a0;
-        public static final int zxing_preview_failed = 0x7f0700a1;
-        public static final int zxing_prewiew_size_ready = 0x7f0700a2;
-        public static final int zxing_status_view = 0x7f0700a3;
-        public static final int zxing_viewfinder_view = 0x7f0700a4;
+        public static final int fitCenter = 0x7f07003f;
+        public static final int fitXY = 0x7f070040;
+        public static final int zxing_back_button = 0x7f07009b;
+        public static final int zxing_barcode_scanner = 0x7f07009c;
+        public static final int zxing_barcode_surface = 0x7f07009d;
+        public static final int zxing_camera_closed = 0x7f07009e;
+        public static final int zxing_camera_error = 0x7f07009f;
+        public static final int zxing_decode = 0x7f0700a0;
+        public static final int zxing_decode_failed = 0x7f0700a1;
+        public static final int zxing_decode_succeeded = 0x7f0700a2;
+        public static final int zxing_possible_result_points = 0x7f0700a3;
+        public static final int zxing_preview_failed = 0x7f0700a4;
+        public static final int zxing_prewiew_size_ready = 0x7f0700a5;
+        public static final int zxing_status_view = 0x7f0700a6;
+        public static final int zxing_viewfinder_view = 0x7f0700a7;
     }
     public static final class layout {
         private layout() {}
